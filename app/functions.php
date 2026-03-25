1f9b1ea9f9e00bf217e3b1594ca5f210136cdcc6
ldh791 <50644962+ldh791@users.noreply.github.com>
2026-03-25 09:53:20 +0900

    Update functions.php

--- app/functions.php
+++ app/functions.php
@@ -1,36 +1,5 @@
 <?php
 
-function addPost($board,$content,$image=null){
-  global $db;
-
-  $stmt=$db->prepare("INSERT INTO posts (board,content,image) VALUES (?,?,?)");
-  $stmt->execute([$board,$content,$image]);
-}
-
-function addReply($post_id,$content){
-  global $db;
-
-  $stmt=$db->prepare("INSERT INTO replies (post_id,content) VALUES (?,?)");
-  $stmt->execute([$post_id,$content]);
-}
-
-function getPosts($board){
-  global $db;
-
-  $stmt=$db->prepare("SELECT * FROM posts WHERE board=? ORDER BY id DESC");
-  $stmt->execute([$board]);
-  $posts=$stmt->fetchAll(PDO::FETCH_ASSOC);
-
-  foreach($posts as &$p){
-    $stmt2=$db->prepare("SELECT * FROM replies WHERE post_id=?");
-    $stmt2->execute([$p['id']]);
-    $p['replies']=$stmt2->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  return $posts;
-}
-
-// 썸네일 생성
 function makeThumb($src,$dest,$size){
   $img=imagecreatefromstring(file_get_contents($src));
   $width=imagesx($img);
