1dc92cbd70afcca55949cc4c397d6f943f2fcab2
ldh791 <50644962+ldh791@users.noreply.github.com>
2026-03-25 10:01:42 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,37 @@
-<?php $config=require '../app/config.php'; ?>
+<?php
+
+$uri = trim($_SERVER['REQUEST_URI'], '/');
+
+// 홈
+if($uri == ''){
+?>
 <!DOCTYPE html>
-<html><head><link rel="stylesheet" href="/css/style.css"></head>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="/css/mobile.css">
+</head>
 <body>
-<h1>Imageboard</h1>
-<?php foreach($config['boards'] as $k=>$v): ?>
-<a href="/<?= $k ?>">/<?= $k ?> - <?= $v ?></a><br>
-<?php endforeach; ?>
-</body></html>
+
+<div class="container">
+
+<h1>이미지 게시판</h1>
+
+<ul class="board-list">
+<li><a href="/board.php?board=free">자유게시판</a></li>
+<li><a href="/board.php?board=game">게임게시판</a></li>
+<li><a href="/board.php?board=pic">이미지게시판</a></li>
+</ul>
+
+</div>
+
+</body>
+</html>
+<?php
+exit;
+}
+
+// 게시판 라우팅
+header("Location: /board.php?board=".$uri);
+exit;
