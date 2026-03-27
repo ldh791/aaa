fb23f869b13a3c5efe933e726c55205beaf649f2
ldh791 <50644962+ldh791@users.noreply.github.com>
2026-03-27 09:43:17 +0900

    Update board.php

--- public/board.php
+++ public/board.php
@@ -1,53 +1,56 @@
 <?php
-require_once("../app/functions.php");
 
-$posts = [];
+require_once "../app/functions.php";
 
-if(file_exists("../data/posts.json")){
-    $posts = json_decode(file_get_contents("../data/posts.json"), true);
+$posts = loadPosts();
+
+/* posts가 null이면 빈 배열 */
+if(!$posts){
+    $posts = [];
 }
+
+$posts = array_reverse($posts);
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+
 <meta charset="UTF-8">
 <title>Board</title>
+
 <link rel="stylesheet" href="css/style.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 
 <body>
 
-<div class="container">
-
 <h1>Image Board</h1>
 
-<a class="writebtn" href="index.php">글쓰기</a>
+<a href="index.php">글쓰기</a>
 
 <div class="board">
 
-<?php foreach(array_reverse($posts) as $post){ ?>
+<?php foreach($posts as $post){ ?>
 
-<div class="post">
+<div class="card">
 
-<a href="../uploads/<?php echo $post['image']; ?>" target="_blank">
-<img class="thumb" src="../uploads/<?php echo $post['image']; ?>">
+<a href="board.php?id=<?=$post['id']?>">
+
+<p>No.<?=$post['id']?></p>
+
+<?php if($post['image']){ ?>
+<img src="uploads/<?=$post['image']?>" class="thumb">
+<?php } ?>
+
 </a>
 
-<div class="postinfo">
-<a class="postno" href="../uploads/<?php echo $post['image']; ?>" target="_blank">
-No.<?php echo $post['id']; ?>
-</a>
-
-<p><?php echo htmlspecialchars($post['text']); ?></p>
-</div>
+<p><?=$post['text']?></p>
 
 </div>
 
 <?php } ?>
-
-</div>
 
 </div>
 
