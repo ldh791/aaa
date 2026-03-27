ce474743584a789e9112ba0270e1872a2811c044
ldh791 <50644962+ldh791@users.noreply.github.com>
2026-03-27 09:08:44 +0900

    Delete app directory

--- app/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-  'boards' => ['b'=>'Random','g'=>'Game'],
-  'admin_pass' => '1234'
-];
--- app/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-function loadPosts(){
-  if(!file_exists(__DIR__.'/../storage.json')) return [];
-  return json_decode(file_get_contents(__DIR__.'/../storage.json'), true);
-}
-function savePosts($posts){
-  file_put_contents(__DIR__.'/../storage.json', json_encode($posts));
-}
